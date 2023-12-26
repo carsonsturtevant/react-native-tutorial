5c74d450761e50e9221983f4378cb7bc7f988e2b
Carson <carson.sturtevant@gmail.com>
2023-12-26 15:30:40 -0700

    React Native tutorial

---
+++ components/ImageViewer.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Image } from 'react-native';
+
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+    const imgSrc = selectedImage ? {uri: selectedImage} : placeholderImageSource;
+    return (
+        <Image source={imgSrc} style={styles.image} />
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
+});
